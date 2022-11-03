8158c4aa11a29fb99bf71e485322644d58b01da3
AdriaGarcia1 <adrigarla@gmail.com>
2022-11-03 19:25:38 +0100

    commit

---
+++ Practica2/Pagines/Pagina3/js.js
@@ -0,0 +1,17 @@
+function f_hover(){
+    document.body.style.backgroundImage = "url(../../Imatges/FEID-10.webp)";
+    document.body.style.backgroundImage.repeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "center";
+    document.body.style.filter = "grayscale()";
+    document.body.style.opacity = ".5";
+}
+
+function f_hover2(){
+    document.body.style.backgroundImage = "url(../../Imatges/Reggaeton-Beach-Festival-mainstage-min.jpg)";
+    document.body.style.backgroundImage.repeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "center";
+    document.body.style.filter = "grayscale()";
+    document.body.style.opacity = ".5";
+}
